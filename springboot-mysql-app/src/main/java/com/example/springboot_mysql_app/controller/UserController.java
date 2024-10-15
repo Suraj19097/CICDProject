659d323a1553822244780cfe8c5075fe2f69ba7a
suraj <suraj@gmail.com>
2024-10-15 17:21:59 +0530

    my first commit

---
+++ springboot-mysql-app/src/main/java/com/example/springboot_mysql_app/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.springbootapp.controller;
+
+import com.example.springbootapp.model.User;
+import com.example.springbootapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+}
---
+++ springboot-mysql-app/src/main/java/com/example/springboot_mysql_app/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.springbootapp.service;
+
+import com.example.springbootapp.model.User;
+import com.example.springbootapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
